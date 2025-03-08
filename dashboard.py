bffaa24f501093ec854b67928a7ec425437fa1b6
lexyminaire <alekpiterw1@gmail.com>
2025-03-08 17:33:12 +0700

    modified:   dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -137,19 +137,19 @@ fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 6))
 
 colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
 
-sns.barplot(y="recency", x="registered", data=rfm.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+sns.barplot(y="recency", x="registered", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel(None)
 ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
 ax[0].tick_params(axis ='x', labelsize=15)
 
-sns.barplot(y="frequency", x="registered", data=rfm.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+sns.barplot(y="frequency", x="registered", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
 ax[1].set_xlabel(None)
 ax[1].set_title("By Frequency", loc="center", fontsize=18)
 ax[1].tick_params(axis='x', labelsize=15)
 
-sns.barplot(y="monetary", x="registered", data=rfm.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+sns.barplot(y="monetary", x="registered", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
 ax[2].set_ylabel(None)
 ax[2].set_xlabel(None)
 ax[2].set_title("By Monetary", loc="center", fontsize=18)
